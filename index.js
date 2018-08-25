39201b9830b3285823f86ace0f831da77b66c497
mynameisbond <hajdinim@uni.coventry.ac.uk>
2018-08-25 09:42:32 +0100

    redux cleaner

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
 
+const messanger = require('./route/messages');
+const profile = require('./route/profile');
+const logreg = require('./route/log-reg');
+const blog = require('./route/blog');
+// Database
+const User = require('./models/User');
+const db = mongoose.connection;
+mongoose.connect('mongodb://localhost/user');
+
+db.on('error', err => {
+  throw err;
+});
+
+db.once('open', () => {
+  console.log('MongoDB connected');
+});
 
 app.get('/', (req, res) => {
-    console.log('Hello!')
-})
-
+  User.find({}, (err, user) => {
+    if (err) throw err;
+    return res.send(user);
+  });
+});
 
 app.listen(8080, () => {
-    console.log(`The server is running in ${8080} port.`)
-})
+  console.log(`The server is running in ${8080} port.`);
+});
+app.use('/messages', messanger);
+app.use('/profile', profile);
+app.use('/login', logreg);
+app.use('/blog', blog);
