c561c2d402086aeb518184bda84c0ad43ba8a05e
mynameisbond <hajdinim@uni.coventry.ac.uk>
2018-08-30 02:56:22 +0100

    register form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import App from '../styles/App.css'
-import { Field, reduxForm } from 'redux-form'
-import { Icon } from 'react-materialize'
-
+import React from 'react';
+import App from '../styles/App.css';
+import { Field, reduxForm } from 'redux-form';
+import { Icon } from 'react-materialize';
+import { isNotEmail } from 'sane-email-validation';
 const validate = values => {
-  const errors = {}
+  const errors = {};
   if (!values.password) {
-    errors.password = 'Required'
+    errors.password = 'Required';
   }
   if (!values.email) {
-    errors.email = 'Required'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address'
+    errors.email = 'Required';
+  } else if (isNotEmail(values.email)) {
+    errors.email = 'Invalid email address';
   }
 
-  return errors
-}
+  return errors;
+};
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
   <div>
     <label>{label}</label>
     <div>
@@ -32,30 +37,44 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
           (warning && <span>{warning}</span>))}
     </div>
   </div>
-)
+);
 
 const SyncValidationForm = props => {
-  const { handleSubmit, pristine, reset, submitting } = props
+  const { handleSubmit, pristine, reset, submitting } = props;
   return (
     <div className="container login_style" style={App.something}>
       <h4>LOGIN</h4>
       <div className="login_inner">
         <form onSubmit={handleSubmit}>
           <Icon>mail</Icon>
-          <Field name="email" type="email" component={renderField} label="Email" />
+          <Field
+            name="email"
+            type="email"
+            component={renderField}
+            label="Email"
+          />
           <Icon>lock</Icon>
-          <Field name="password" type="password" component={renderField} label="Password" />
-          <button type="submit" className="waves-effect waves-light btn" disabled={submitting}>
+          <Field
+            name="password"
+            type="password"
+            component={renderField}
+            label="Password"
+          />
+          <button
+            type="submit"
+            className="waves-effect waves-light btn"
+            disabled={submitting}
+          >
             Login
           </button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default reduxForm({
   form: 'SyncValidationForm',
   validate,
   renderField
-})(SyncValidationForm)
+})(SyncValidationForm);
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { isNotEmail } from 'sane-email-validation';
 import { Row, Input, Icon, Button } from 'react-materialize';
 
 const Ad_card = () => {
@@ -46,77 +48,80 @@ const Advertisement1 = () => {
   );
 };
 
-const Register = () => {
-  return (
-    <div>
-      <div className="container login_style">
-        <Row>
-          <h2>Register</h2>
-          <small>* are mendatory</small>
-          <br />
-          <Row>
-            <Input s={12} label="Username*" validate>
-              <Icon>account_circle</Icon>
-            </Input>
-          </Row>
-          <Row>
-            <Input s={12} label="email*" type="email" validate>
-              <Icon>email</Icon>
-            </Input>
-          </Row>
-          <Row>
-            <Input s={12} label="password*" type="password" validate>
-              <Icon>vpn_key</Icon>
-            </Input>
-          </Row>
-          <Row>
-            <Input s={12} label="repeat password*" type="password" validate>
-              <Icon>vpn_key</Icon>
-            </Input>
-          </Row>
-          <Row>
-            <Input
-              s={12}
-              name="on"
-              type="date"
-              label="Date of Birth*"
-              icon="date_range"
-              onChange={function(e, value) {}}
-            />
-          </Row>
-          <Row>
-            <Input
-              s={12}
-              type="select"
-              label="Select you sex"
-              icon="wc"
-              defaultValue="2"
-            >
-              <option value="1">Male</option>
-              <option value="2">Female</option>
-              <option value="3">Gay</option>
-            </Input>
-          </Row>
-        </Row>
+const validate = values => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = 'Required';
+  } else if (values.username.length > 15) {
+    errors.username = 'Must be 15 characters or less';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (isNotEmail(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length <= 7) {
+    errors.password = 'Password should be more than 8 characters';
+  }
+  return errors;
+};
 
-        <Row>
-          <small>What gender you want your matches to be?*</small>
-          <Input name="group1" type="checkbox" value="Males" label="Males" />
-          <Input
-            name="group1"
-            type="checkbox"
-            value="Females"
-            label="Females"
-          />
-          <Input name="group1" type="checkbox" value="Gay" label="Gay" />
-        </Row>
-        <Button waves="light"> Register </Button>
-      </div>
-      <div>
-        <Advertisement1 />
-      </div>
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && (
+          <div className="error_div">
+            <Icon>report</Icon>
+            <span>{error}</span>
+          </div>
+        )) ||
+          (warning && <span>{warning}</span>))}
     </div>
+  </div>
+);
+
+const Regiser = props => {
+  const { handleSubmit, pristine, reset, submitting } = props;
+  return (
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="username"
+        type="text"
+        component={renderField}
+        label="Username"
+      />
+      <Field name="email" type="email" component={renderField} label="Email" />
+      <Field
+        name="password"
+        type="password"
+        component={renderField}
+        label="password"
+      />
+      <Field
+        name="password2"
+        type="password"
+        component={renderField}
+        label="Repeat Password"
+      />
+      <button type="submit" disabled={submitting}>
+        Register
+      </button>
+    </form>
   );
 };
 
-export default Register;
+export default reduxForm({
+  form: 'syncValidation', // a unique identifier for this form
+  validate, // <--- validation function given to redux-form
+  renderField // <--- warning function given to redux-form
+})(Regiser);
