ed50d3b6b30ca87764722480ffede50ab9384905
mynameisbond <hajdinim@uni.coventry.ac.uk>
2018-09-01 12:46:42 +0100

    messages-cc

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
 const Chat_Grid = () => <div id="chat-grid">chat grid</div>
-const 
-
+const Message_fiels = () => <div id="message-fields">message fields</div>
+const Message_infos = () => <div id="message-infos">message infos</div>
+const Message_input = () => <div id="message-input">message input</div>
+const Message_search = () => <div id="message-search">message search</div>
 
 const Messages = () => (
   <div className="messages-grid">
     <Chat_Grid />
+    <Message_fiels />
+    <Message_infos />
+    <Message_input />
+    <Message_search />
   </div>
 )
 
