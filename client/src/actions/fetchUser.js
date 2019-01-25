9ab95429874da74ef9402aceeb24561cd961322f
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2019-01-25 22:15:50 +0000

    test

--- client/src/actions/fetchUser.js
+++ client/src/actions/fetchUser.js
@@ -4,7 +4,7 @@ import { showLoading, hideLoading } from 'react-redux-loading-bar'
 
 export const fetchUser = () => async (dispatch, props) => {
   dispatch(showLoading())
-  // const userID = await axios.get(`/profile/${userID}`)
+  const userID = await axios.get(`/profile/${userID}`)
   const us = props.getState().user
   console.log(us)
   dispatch({ type: FETCH_USER, payload: '5bcc0dfffd6bfc15b19ab6a3' })
